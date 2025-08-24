9f22a38a69a0ec44a076e76f83eb8c35db6079e5
Puuthai <phum29.wkt@gmail.com>
2025-08-24 18:20:27 +0700

    24aug25 admin-update

---
+++ include/footer_admin.php
@@ -0,0 +1,3 @@
+
+<script src="../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/custom/script.js"></script>
---
+++ include/header_admin.php
@@ -0,0 +1,5 @@
+
+<link rel="stylesheet" href="../assets/custom/stlye.css">
+<link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="../assets/bootstrap-icons/font/bootstrap-icons.css">
+
